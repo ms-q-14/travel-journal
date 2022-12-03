740f0df81093904902b12d9cf7fb2148349645d8
Saad Qureshi <muhammad.saadq14@gmail.com>
2022-12-03 01:04:09 -0500

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <img
+        src="https://www.airbelgium.com/wp-content/uploads/2021/10/Punta-Cana-2-1.jpg"
+        className="cardImg"
+      />
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <i class="bi bi-globe-americas"></i>
+      <h1 className="header__title">My Travel Journal</h1>
+    </div>
+  );
+}
+
+export default Header;
